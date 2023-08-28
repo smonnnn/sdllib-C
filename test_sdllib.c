47e575a9ba97173f81a4b1bd9bca6fff2baa9c0c
smonnnn <slooij1@gmail.com>
2023-08-28 16:36:53 +0200

    Remove old version.

--- test_sdllib.c
+++ test_sdllib.c
@@ -7,8 +7,8 @@ int main(){
     srand(time(NULL));
     printf("Starting!\n");
     SDLNet network;
-    int layer_sizes[] = {3, 3, 2};
-    init_network(&network, layer_sizes, 3);
+    int layer_sizes[] = {3, 10, 50, 100, 50, 10, 2};
+    init_network(&network, layer_sizes, 7);
 
     Matrix input = mat_new(1, 3);
     mat_set(&input, 0, 0, 0.5f);
@@ -20,10 +20,10 @@ int main(){
 
     printf("Training!\n");
     int i = 0;
+    
     while(network.total_error > 0.01f){
-        printf("E:%i\n", i);
         backward(&network, &input, &target);
-        printf("Total error: %7.2f\n", network.total_error);
+        printf("%4i error: %7.2f\n", i, network.total_error);
         i++;
     }
     
